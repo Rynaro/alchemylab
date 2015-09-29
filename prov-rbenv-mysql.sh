905d156d02fbb9b4793b3a6d11bc03159e400285
Rynaro <henlavezzo@gmail.com>
2015-09-29 01:11:38 -0300

    Remove strange I inside file

--- prov-rbenv-mysql.sh
+++ prov-rbenv-mysql.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
-I
+
 sudo -u vagrant git clone git://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.bashrc
 echo 'eval "$(rbenv init -)"' >> /home/vagrant/.bashrc
